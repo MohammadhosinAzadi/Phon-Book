6dbc6cc053f39c475c2d3f81ac1d048b70ec7ef4
Mohammadhosin Aazdi <164636195+MohammadhosinAzadi@users.noreply.github.com>
2024-07-29 17:18:01 +0330

    phone-book task

---
+++ phon-book/src/index.ts
@@ -0,0 +1,88 @@
+import { toNamespacedPath } from 'path';
+import data from '../phonbook.json';
+
+interface UserPhone { 
+    name : string;
+    phone : string
+}
+const user: UserPhone[] = data;
+
+
+const searchUser: string[] = process.argv.slice(2);
+
+function searchFunction(args: string[] | undefined) {
+    args = searchUser;
+
+    args.forEach((name, phone) => {
+        if (user.some((x) => x.name === name) || user.some((x) => x.phone === phone)) {
+            console.log("Error : This information is in the phone book");
+        }   
+    })
+
+    args.forEach((name, phone) => {
+        if (!user.some((X) => X.name === name) || !user.some((X) => X.phone === phone)) {
+            user.push({name, phone: "unknown"})
+        }
+    })
+}
+
+
+console.log(searchFunction(searchUser));
+
+
+console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+ 
+
+
