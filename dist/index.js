3e66346d3f3f5b3c8e17e88c2ec6c9514514046c
Mohammadhosin Aazdi <164636195+MohammadhosinAzadi@users.noreply.github.com>
2024-08-04 19:56:17 +0330

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,1 @@
-"use strict";
-console.log("hello world");
-console.log("languach typeScript");
-let age = 18;
-if (age < 20) {
-    age = age + 20;
-    console.log(age);
-}
-let num = 123456789;
-let str = "salam";
-console.log(num);
-console.log(str);
-console.log("11111222222");
-//# sourceMappingURL=index.js.map
+
